6103b7ddcba58cc3bf5c9d1f374f2d48dee439d3
mhdmubashir <msfiuml2@gmail.com>
2024-02-23 22:07:48 +0530

    auth gate changed

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,28 @@
+import 'package:amolchat/auth/login_or_register.dart';
+import 'package:amolchat/pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // userisloggedin
+          if (snapshot.hasData) {
+            return const HomePage();
+          }
+
+          // userNOT loggdin
+          else {
+            return const LoginOrRegister();
+          }
+        },
+      ),
+    );
+  }
+}
